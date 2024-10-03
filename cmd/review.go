73151df1fdfe7a46c4aa57aaf25fcd698783442e
Matt Gottlieb <mattgottlieb96@gmail.com>
2024-10-02 22:18:24 -0700

    Refactor review.go for better maintainability and optimized flag checks.

--- cmd/review.go
+++ cmd/review.go
@@ -11,20 +11,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var repository string
-var repositoryOwner string
-var commitSHA string
-var prNumber int
-var printOnly bool
-var llmEndpoint string
-var workflowName string
+var branch string
+var fileRegexPattern string
+var llmRetries int
 
-// reviewCmd represents the review command
-var reviewCmd = &cobra.Command{
-	Use:   "review",
-	Short: "Review a pull request",
-	// 	Long: `A longer description that spans multiple lines and likely contains examples
-	// and usage of using your command. For example:
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new pull request",
+	Long:  `Create a new pull request`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		err := app.CheckGHToken()
 		if err != nil {
@@ -38,13 +33,6 @@ var reviewCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
-		if commitSHA == "" {
-			logger.Fatal().Msg("commitSHA is required")
-		}
-		if prNumber == 0 {
-			logger.Fatal().Msg("prNumber is required and should be greater than 0")
-		}
-
 		application := app.SetupNoEnv(&config.Config{
 			Log: config.Log{
 				Level: zerolog.InfoLevel,
@@ -60,47 +48,34 @@ var reviewCmd = &cobra.Command{
 				Endpoint:     llmEndpoint,
 			},
 		})
-		err := application.ExecutePRReview(context.Background(), commitSHA, prNumber, workflowName, printOnly)
+		err := application.ExecuteCreatePR(context.TODO(), commitSHA, branch, workflowName, fileRegexPattern, printOnly)
 		if err != nil {
-			logger.Fatal().Err(err).Msg("Error executing PR review")
+			logger.Fatal().Err(err).Msg("Error executing Create PR")
 		}
 	},
 }
 
 func init() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
-	rootCmd.AddCommand(reviewCmd)
+	rootCmd.AddCommand(createCmd)
 
-	reviewCmd.Flags().StringVar(&repository, "repository", "", "Github repository to review such as snappr (required)")
-	reviewCmd.Flags().StringVar(&repositoryOwner, "repositoryOwner", "", "The account owner of the repository. The name is not case sensitive. (required)")
-	reviewCmd.Flags().StringVar(&commitSHA, "commitSHA", "", "Commit SHA to review (required)")
-	reviewCmd.Flags().IntVar(&prNumber, "prNumber", 0, "Pull Request number to review (required)")
-	reviewCmd.Flags().BoolVarP(&printOnly, "printOnly", "p", false, "Print the review only")
-	reviewCmd.Flags().StringVar(&llmEndpoint, "llmEndpoint", "", "Endpoint for the LLM service (defaults to OpenAI)")
-	reviewCmd.Flags().IntVarP(&llmRetries, "llmRetries", "r", 3, "Number of retries for LLM API calls when failing to get a valid llm response")
+	createCmd.Flags().StringVar(&repository, "repository", "", "Github repository to review such as snappr (required)")
+	createCmd.Flags().StringVar(&repositoryOwner, "repositoryOwner", "", "The account owner of the repository. The name is not case sensitive. (required)")
+	createCmd.Flags().StringVar(&commitSHA, "commitSHA", "", "Commit SHA to create PR from (required)")
+	createCmd.Flags().StringVar(&branch, "branch", "", "Branch name to create PR from (required)")
+	createCmd.Flags().BoolVarP(&printOnly, "printOnly", "p", false, "Print the created PR only")
+	createCmd.Flags().StringVar(&fileRegexPattern, "fileRegexPattern", `.*\.go$`, "Define a regex pattern to filter files to use as context for PR creation")
+	createCmd.Flags().StringVar(&llmEndpoint, "llmEndpoint", "", "Endpoint for the LLM service (defaults to OpenAI)")
+	createCmd.Flags().IntVarP(&llmRetries, "llmRetries", "r", 3, "Number of retries for LLM API calls when failing to get a valid llm response")
 
-	err := reviewCmd.MarkFlagRequired("repository")
-	if err != nil {
-		logger.Fatal().Err(err).Msg("Error marking repository as required")
-	}
+	setRequiredFlags(createCmd, []string{"repository", "repositoryOwner", "commitSHA", "branch", "workflowName"})
+}
 
-	err = reviewCmd.MarkFlagRequired("repositoryOwner")
-	if err != nil {
-		logger.Fatal().Err(err).Msg("Error marking repositoryOwner as required")
+func setRequiredFlags(cmd *cobra.Command, flags []string) {
+	for _, flag := range flags {
+		err := cmd.MarkFlagRequired(flag)
+		if err != nil {
+			logger.Fatal().Err(err).Msgf("Failed to mark '%s' as required", flag)
+		}
 	}
-
-	err = reviewCmd.MarkFlagRequired("commitSHA")
-	if err != nil {
-		logger.Fatal().Err(err).Msg("Error marking commitSHA as required")
-	}
-	err = reviewCmd.MarkFlagRequired("prNumber")
-	if err != nil {
-		logger.Err(err).Msg("Error marking prNumber as required")
-	}
-	reviewCmd.Flags().StringVar(&workflowName, "workflowName", "", "Prompt workflow to use (required)")
-	err = reviewCmd.MarkFlagRequired("workflowName")
-	if err != nil {
-		logger.Err(err).Msg("Error marking workflowName as required")
-	}
-
 }
