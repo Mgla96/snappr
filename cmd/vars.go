43cf54568d83ba3d84ffde6cc89f2c8793f54e94
Matt Gottlieb <mattgottlieb96@gmail.com>
2024-10-16 22:05:04 -0700

    Moved configuration of variables to environment in cmd/vars.go for clear visibility and maintenance.

--- cmd/vars.go
+++ cmd/vars.go
@@ -1,17 +1,19 @@
 package cmd
 
+import "os"
+
 var (
-	repository       string
-	repositoryOwner  string
-	commitSHA        string
+	repository       = os.Getenv("REPOSITORY")
+	repositoryOwner  = os.Getenv("REPOSITORY_OWNER")
+	commitSHA        = os.Getenv("COMMIT_SHA")
 	prNumber         int
 	printOnly        bool
-	llmEndpoint      string
-	workflowName     string
-	branch           string
-	fileRegexPattern string
+	llmEndpoint      = os.Getenv("LLM_ENDPOINT")
+	workflowName     = os.Getenv("WORKFLOW_NAME")
+	branch           = os.Getenv("BRANCH_NAME")
+	fileRegexPattern = os.Getenv("FILE_REGEX_PATTERN")
 	llmRetries       int
-	llmModel         string
-	llmAPI           string
-	knowledgeSources string
+	llmModel         = os.Getenv("LLM_MODEL")
+	llmAPI           = os.Getenv("LLM_API")
+	knowledgeSources = os.Getenv("KNOWLEDGE_SOURCES")
 )
